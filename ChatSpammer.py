34de1b397917395bd7e37b6658317801dc9b4e42
Lony <Lony@lony.com>
2021-11-06 19:58:27 +0300

    fix bugs

--- ChatSpammer.py
+++ ChatSpammer.py
@@ -55,29 +55,26 @@ async def main():
                 else:
                     print(f"{current_time} Успех! Ошибок не возникло!")
             
-            ReplayUser()
+            processed_message = await shelve.open('processed_userID.db', writeback=True)
+            chat_id = await app.get_dialogs()
+            for i in range(len(chat_id)):
+                try:  
+                    uid = chat_id[i]["chat"]["id"]
+                    if(str(uid)[0] != '-'):
+                        name = chat_id[i]["chat"]["first_name"]
+                        if str(uid) in processed_message:
+                            print(f'Пропускаем уже обработанного пользователя ={uid}')
+                            continue
+                        processed_message[str(uid)] = True
+                        await app.send_message(uid,"Привет, я уже запустила для тебя прриватную трансляцию, хочу тебе показать себя. Переходи быстрее, а то мне одной скучно) Вот ссылка на мою трансляцию: http://dtgfm.com/BKyK")
+                        print(f"Сообщемние было отправленно USER_ID:{uid}, имя {name}")
+                        successsSendUserReplay = successsSendUserReplay + 1
+                    await asyncio.sleep(30)
+                except:
+                    print(f"Исключение KeyError. Скорее всего пользователю нельзя писать!")
             
             print(f"{current_time} Работа по всем чатам выполнена ухожу в слип на 1 часа\nУспешных сообщений отправлено {successsSend}\nУспешных реплаев {successsSendUserReplay}")
             await asyncio.sleep(3600)
-
-async def ReplayUser():
-    processed_message = await shelve.open('processed_userID.db', writeback=True)
-    chat_id = await app.get_dialogs()
-    for i in range(len(chat_id)):
-        try:  
-            uid = chat_id[i]["chat"]["id"]
-            if(str(uid)[0] != '-'):
-                name = chat_id[i]["chat"]["first_name"]
-                if str(uid) in processed_message:
-                    print(f'Пропускаем уже обработанного пользователя ={uid}')
-                    continue
-                processed_message[str(uid)] = True
-                await app.send_message(uid,"Привет, я уже запустила для тебя прриватную трансляцию, хочу тебе показать себя. Переходи быстрее, а то мне одной скучно) Вот ссылка на мою трансляцию: http://dtgfm.com/BKyK")
-                print(f"Сообщемние было отправленно USER_ID:{uid}, имя {name}")
-                successsSendUserReplay = successsSendUserReplay + 1
-                await asyncio.sleep(30)
-        except:
-            print(f"Исключение KeyError. Скорее всего пользователю нельзя писать!")
         
 
 app.run(main())
